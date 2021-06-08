800cf4f98ef9d3a5fa5702a900ae77bb22f4c14a
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-07 21:14:51 -0700

    changed getTasks to stream, added update functions

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,14 +1,43 @@
 const { Op } = require('sequelize');
 const { Task } = require('../model');
+const { sendTaskUpdates } = require('../middleware');
+
+let clients = [];
 
 module.exports = {
+  clients,
   getTasks: async (req, res) => {
     const tasks = await Task.findAll();
-    res.render('tasks', { tasks });
+
+    res.status(200).set({
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-control': 'no-cache',
+    });
+
+    const data = `data: ${JSON.stringify(tasks)}\n\n`;
+
+    res.write(data);
+
+    const clientId = Date.now();
+
+    const newClient = {
+      id: clientId,
+      res,
+    };
+
+    clients = [...clients, newClient];
+
+    req.on('close', () => {
+      console.log(`${clientId} Connection closed`);
+      clients = clients.filter((client) => client.id !== clientId);
+    });
   },
   createTask: async (req, res) => {
     if (!req.task) {
       await Task.create(req.query);
+      const tasks = await Task.findAll();
+      sendTaskUpdates(clients, tasks);
       res.status(201).json({ message: 'Your task is submitted! Get to work!' });
     } else res.status(200).json({ message: 'You must finish your open task first' });
   },
@@ -19,6 +48,8 @@ module.exports = {
       await req.task.update({ finished: true });
       message = 'Nailed it! Look at you go!';
       status = 201;
+      const tasks = await Task.findAll();
+      sendTaskUpdates(clients, tasks);
     }
     res.status(status).json({ message });
   },
@@ -39,7 +70,8 @@ module.exports = {
     } else {
       res.status(200).json({ message: 'Unauthorized. You can only delete your own tasks.' });
     }
-
+    const tasks = await Task.findAll();
+    sendTaskUpdates(clients, tasks);
     res.status(201).json({ message: 'Your task has been removed' });
   },
   getUnfinishedTask: async (req, res, next) => {
@@ -56,6 +88,9 @@ module.exports = {
     if (req.query.user !== 'theDabolical')
       res.json({ message: 'Only the stream owner can delete all tasks.' });
     await Task.sync({ force: true });
+
+    const tasks = await Task.findAll();
+    sendTaskUpdates(clients, tasks);
     res.status(201).json({ message: 'All tasks reset.' });
   },
 };
