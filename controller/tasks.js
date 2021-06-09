c0b30320e0d2bb7f99de605ef16a1efc12c722ea
James Crosslin <jamesmscrosslin@gmail.com>
2021-06-08 18:52:48 -0700

    replacing validator lib with custom sanitization

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,6 @@
 const { Op } = require('sequelize');
 const { Task } = require('../model');
 const { sendTaskUpdates } = require('../middleware');
-const validator = require('validator');
 
 let clients = [];
 
@@ -13,8 +12,8 @@ module.exports = {
       return {
         id,
         finished,
-        user: validator.blacklist(user, '[<>/\\()]'),
-        task: validator.blacklist(task, '[<>/\\();^]'),
+        user: user.replace(/[<>/\\()]/g, ' '),
+        task: task.replace(/[<>/\\()]/g, ' '),
       };
     });
 
